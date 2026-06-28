933a574d2e9f9a82e8e57c500ae05d721c6fb8d0
David Bone <davidbone2004@gmail.com>
2026-06-28 09:31:32 -0500

    Main singleton y estructuramiento Factory

---
+++ src/singleton/MainSingleton.java
@@ -0,0 +1,23 @@
+package singleton;
+
+public class MainSingleton {
+
+    public static void main(String[] args) {
+        System.out.println("=== PATRÓN SINGLETON ===\n");
+
+
+        ConfiguracionSistema c1 = ConfiguracionSistema.getInstancia();
+        ConfiguracionSistema c2 = ConfiguracionSistema.getInstancia();
+
+
+        System.out.println("c1 == c2: " + (c1 == c2));
+        System.out.println("Configuración inicial: " + c1);
+
+        c1.setHost("192.168.1.10");
+        c1.setPuerto(9090);
+        System.out.println("\nDespués de modificar c1:");
+        System.out.println("  c1: " + c1);
+        System.out.println("  c2: " + c2);
+        System.out.println("  (c1 y c2 apuntan al mismo objeto, por eso c2 también cambió)");
+    }
+}
